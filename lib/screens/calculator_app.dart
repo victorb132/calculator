bcb83df43458d2529d5b06cfa36736c4e1b8fa3d
Victor Novais <victor96novais@gmail.com>
2023-11-04 23:57:56 -0300

    feat/calculator-finished

---
+++ lib/screens/calculator_app.dart
@@ -0,0 +1,42 @@
+import 'package:calculator/components/display.dart';
+import 'package:calculator/components/keyboard.dart';
+import 'package:calculator/models/memory.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class CalculatorApp extends StatefulWidget {
+  const CalculatorApp({super.key});
+
+  @override
+  State<CalculatorApp> createState() => _CalculatorAppState();
+}
+
+class _CalculatorAppState extends State<CalculatorApp> {
+  final Memory memory = Memory();
+
+  _onPressed(String command) {
+    setState(() {
+      memory.applyCommand(command);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]);
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: false,
+      ),
+      home: Column(
+        children: [
+          Display(text: memory.value),
+          Keyboard(callback: _onPressed),
+        ],
+      ),
+    );
+  }
+}
